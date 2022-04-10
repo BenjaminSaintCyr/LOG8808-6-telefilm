c454b9f87e0d11a060ad8f3515b3e0fd38c0e31e
mafil2 <marc-andre.filion@bell.net>
2022-04-10 13:34:36 -0400

    language according to city preprocess

--- Projet/src/scripts/preprocess.js
+++ Projet/src/scripts/preprocess.js
@@ -1,5 +1,3 @@
-import { provinces } from "./constants"
-
 export function getLanguageLineChartData(data, province='None') {
     let newData = []
 
@@ -27,14 +25,24 @@ export function getLanguageLineChartData(data, province='None') {
 }
 
 export function getLanguagePieChartData(data) {
-    let newData = { "provinces": [{ province: { "villes": [{ ville: {"nbFrancais": 0, "nbAnglais": 0}}] } }]}
+    let newData = { "provinces": new Map()}
 
     data = data.filter(element => {
-        return element.pays == 'Canada' 
+        return element.pays === 'Canada' 
     })
     
     data.forEach(({ province, langue, ville }) => {
-        newData.push({ province, ville})
+        // Data contains mix of accents and lower/upper cased cities names... 
+        const upperCasedCityNoAccents = ville.toUpperCase().normalize("NFD").replace(/\p{Diacritic}/gu, "") 
+        
+        if (!newData.provinces.has(province)) {
+            newData.provinces.set(province, { 'villes': new Map() } );
+        } if (!newData.provinces.get(province).villes.has(upperCasedCityNoAccents)) {
+            newData.provinces.get(province).villes.set(upperCasedCityNoAccents, {"nbFrancais": 0, "nbAnglais": 0});
+        }
+        langue === 'Français' ?
+            newData.provinces.get(province).villes.get(upperCasedCityNoAccents).nbFrancais++ :
+            newData.provinces.get(province).villes.get(upperCasedCityNoAccents).nbAnglais++
     })
 
     return data
